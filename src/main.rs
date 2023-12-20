91b9678ef8ecfacfd9cb3d3b4c1926af4b830726
Random Long Int <pierrelouislet@gmail.com>
2023-12-20 01:02:45 -0500

    feat: provide simple lexer usage example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use minicss::{LexError, Lexer};
+
 fn main() {
-    println!("Hello le Monde!");
+    let css_input = r#"main {
+        color: red;
+        background-color: #fff;
+        border: 1px solid black;
+        /* A comment */
+        font-size: 1.25em;
+    }"#;
+
+    let mut lexer = Lexer::new(css_input);
+
+    loop {
+        match lexer.next_token() {
+            Ok(token) => {
+                println!("Token: {:?}", token);
+
+                // TODO !!
+                // Impl EOF Token for clean breaking loop.
+
+                // if token.kind == TokenKind::EOF {
+                //     break;
+                // }
+            }
+            Err(LexError::EndOfFile) => {
+                println!("End of file reached.");
+                break;
+            }
+            Err(e) => {
+                eprintln!("Error occurred during lexing: {:?}", e);
+                break;
+            }
+        }
+    }
 }
