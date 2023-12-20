07548159761d3060b8f0761736a5a8fddf77104d
Pierre-Louis <78484833+PierreLouisLetoquart@users.noreply.github.com>
2023-12-20 01:06:04 -0500

    fix: remove useless comments

--- src/error.rs
+++ src/error.rs
@@ -1,33 +1,20 @@
 #[derive(Debug)]
 pub enum LexError {
-    /// Represents an encountered unexpected character.
     UnexpectedCharacter {
-        /// The unexpected character that was encountered.
         character: char,
-        /// The line where the unexpected character was found.
         line: usize,
-        /// The column where the unexpected character was found.
         column: usize,
     },
-    /// Represents a generic lexing error with a custom message.
     GenericError {
-        /// The message describing the error.
         message: String,
     },
-    /// Represents an error due to reaching the end of the file unexpectedly.
     EndOfFile,
-    /// Represents an error due to an unclosed string literal.
     UnclosedString {
-        /// The line where the unclosed string starts.
         line: usize,
-        /// The column where the unclosed string starts.
         column: usize,
     },
-    /// Represents an error due to an unclosed comment.
     UnclosedComment {
-        /// The line where the unclosed comment starts.
         line: usize,
-        /// The column where the unclosed comment starts.
         column: usize,
     },
 }
